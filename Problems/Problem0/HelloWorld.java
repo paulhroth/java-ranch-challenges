1f17706c4c85b906f93f4de3754be901b7bac1a9
Paul <paul.roth@polarmobile.com>
2013-06-05 01:32:54 -0400

    added problem description to HelloWorld.java in Problem0

--- Problems/Problem0/HelloWorld.java
+++ Problems/Problem0/HelloWorld.java
@@ -1,3 +1,5 @@
+//Problem: Write a program that can, when run, print a string.
+
 public class HelloWorld{
   public static void main ( String[] args ) {
     System.out.println("You, madame/sir, are a scholar and gentleperson for reading this!");
