0f531f6e6bf8fc56cdb7a2de171b452cb8a47084
Paul <paul.roth@polarmobile.com>
2013-06-05 21:13:21 -0400

    Problem 2 Completed

--- Problems/Problem1/OneHundredTimes.java
+++ Problems/Problem1/OneHundredTimes.java
@@ -1,6 +1,6 @@
 //Problem: Write a program that will read in a name from the command line and write it out 100 times. 
 
-public class OneHundredTimes{
+public class OneHundredTimes {
   public static void main ( String[] args ) {
     if (args.length != 0) {
       System.out.println("Pleased to meet you, " + args[0] + ". Did you know that I, this source code file, was written on June 5, 01:46 EST Time, 2013?");
---
+++ Problems/Problem2/EvenOrOdd.java
@@ -0,0 +1,27 @@
+//Problem: Write a program that will read in a number from the command line and print out to standard output if it is even or odd.
+
+public class EvenOrOdd {
+  public static void main ( String[] args ) {
+    String parity = "neither even nor odd? How strange.";
+    int number = 0;
+    if (args.length != 1) { 
+      System.out.println("Usage: EvenOrOdd [integer].\nPlease enter a single integer (e.g. -5, 10, 999) as your argument).");
+      System.exit(0);
+    } else {
+      try {
+        number = Integer.parseInt(args[0]);  
+      } catch (Exception e) {
+        System.out.println("Error: please enter a valid integer");
+        System.exit(1);
+      }
+      parity = (number % 2 == 0) ? "even" : "odd";
+      System.out.println("Drumroll..................");
+      try {
+        Thread.sleep(1000);
+      } catch (InterruptedException x) {
+        //ignore
+      }
+      System.out.println("Your number is " + parity + "!!");
+    }
+  }
+}
